ea8bcafeaa006adaf7e1b26c03873fbd2335e406
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-12-16 23:14:58 +0900

    make test code

--- shop/src/main/java/com/shop/repository/ItemRepository.java
+++ shop/src/main/java/com/shop/repository/ItemRepository.java
@@ -3,5 +3,16 @@ package com.shop.repository;
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item,Long> {
+
+    // item 찾기
+    List<Item> findByItemNm(String itemNm);
+
+    // item과 상세설명 Or 조건으로 조회
+   List<Item> findByItemNmOrItemDetail(String itemNm, String ItemDetail);
+
+    // LessThem 조건처리
+    List<Item> findByPriceLessThen(Integer price);
 }
--- shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -34,6 +35,60 @@ class ItemRepositoryTest {
 
         Item saveItem = itemRepository.save(item);
         System.out.println(saveItem.toString());
+    }
+
+    // 테스트 상품 리스트 생성
+    public void  createItemList(){
+        for(int i = 0; i <= 10; i++){
+            Item item = new Item();
+
+            item.setItemNm("테스트상품" + i);
+            item.setPrice(1000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+
+            Item saveItem = itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNmTest(){
+        // 리스트 생성 메소드 실행
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNm("테스트상품1");
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품평, 상품상세설명 or 테스트")
+    public void findByItemNmOrItemDetailTest(){
+
+        // 리스트 생성 메소드 실행
+        this.createItemList();
+
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트상품1" , "테스트 상품 상세 설명5");
+        for (Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+
+    @Test
+    @DisplayName("가격 LessThen 테스트")
+    public void findByPriceLessThenTest(){
+        this.createItemTest();
+
+        List<Item> itemList = itemRepository.findByPriceLessThen(10005);
+
+        for (Item item : itemList){
+            System.out.println(item.toString());
+        }
     }
 
 }
