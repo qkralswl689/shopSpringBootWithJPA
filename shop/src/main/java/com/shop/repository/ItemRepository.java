95ab0e83f582b9927527511e5056980156279595
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-12-23 22:48:05 +0900

    상품 등록 및 조회

--- shop/src/main/java/com/shop/repository/ItemRepository.java
+++ shop/src/main/java/com/shop/repository/ItemRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item,Long> , QuerydslPredicateExecutor<Item> {
+public interface ItemRepository extends JpaRepository<Item,Long> , QuerydslPredicateExecutor<Item> , ItemRepositoryCustom{
 
     // item 찾기
     List<Item> findByItemNm(String itemNm);
